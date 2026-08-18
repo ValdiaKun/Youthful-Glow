a5276f596800ca491f500cf79bfee58f4685b5ac
ValdiaKun <zeethoracle@gmail.com>
2026-08-18 22:12:20 +0800

    Fix V1.3 haptics initializer build error

--- Youthful/Haptics.swift
+++ Youthful/Haptics.swift
@@ -93,9 +93,3 @@ private extension UIControl {
         youthful_sendAction(action, to: target, for: event)
     }
 }
-
-extension ContentView {
-    init() {
-        YouthfulHaptics.install()
-    }
-}
